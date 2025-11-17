f33d5b5339d48cbb7dce5cf32bea9e55b24912c2
ekim3483 <ekim3483@gmail.com>
2025-11-17 12:56:41 -0800

    Extract & consolidate data from csv, json & xml files

--- used-car-prices/etl_code.py
+++ used-car-prices/etl_code.py
@@ -6,7 +6,7 @@ from datetime import datetime
 log_file = "log_file.txt"
 target_file = "transformed_data.csv"
 
-# Functions to extract data from csv, json and xml files
+# Functions to extract data from csv, json & xml files
 def extract_from_csv(file_to_process):
     df = pd.read_csv(file_to_process)
     return df
